a525215273ceae38e700d8cb9ed0f6f9032290c6
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-09-07 11:59:54 +0100

    first commit

---
+++ script.R
@@ -0,0 +1,14 @@
+if (!requireNamespace("digest", quietly = TRUE)) {
+  install.packages("digest")
+}
+
+x <- data.frame(n = 1:26, l = letters)
+
+checksum <- digest::digest(x)
+message("checksum: ", checksum)
+
+expected <- "4c7b13732480ae7154a8f724d99dd1e8"
+message("expected: ", expected)
+
+stopifnot(checksum == expected)
+message("all tests passed!")
